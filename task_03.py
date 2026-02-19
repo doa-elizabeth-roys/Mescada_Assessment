d2f5bb1a4a9128a58a4c49c94ed37b5a4c1e9ba3
Doa Roys <doaelizabethroys@gmail.com>
2026-02-20 00:50:54 +1000

    Add edge case to task 3

--- task_03.py
+++ task_03.py
@@ -1,7 +1,18 @@
-def greet(name):
+user_1 = 'ALICE'
+user_2 = 'BOB'
+
+def greet_user(name):
     print(f"Hello {name}")
 
 print("Enter your name")
 name = input()
-if name == "Alice" or "Bob":
-    greet(name)
+
+while name == "":
+    print("Name cannot be empty")
+    print("Enter your name")
+    name = input()
+
+    if name.upper() == user_1 or name.upper() == user_2:
+        greet_user(name)
+    else:
+        print("\nHello")
