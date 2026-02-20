39a76e27b722cf961bf32c6d5897216a40e0ac69
Doa Roys <doaelizabethroys@gmail.com>
2026-02-20 15:58:37 +1000

    pseudocode for pig latin to english

--- task_20.py
+++ task_20.py
@@ -1,3 +1,5 @@
+#PSEUDOCODE
+
 # function english_to_pigLatin(sentence):
     # Split the sentence into words → word_list
     # Create an empty list → pig_latin_list
@@ -11,3 +13,19 @@
     # Join all words in pig_latin_list with spaces → pig_latin_sentence
     # Return pig_latin_sentence
 
+
+
+# # function pigLatin_to_english(pig_latin_sentence):
+#     Split pig_latin_sentence into words → pigLatin_list
+#     Create empty list → english_list
+
+#     For each word in word_list:
+#         Remove last two characters ("ay") 
+#         Slice the last character of word stored as first_letter
+#         The remaining characters = rest_of_word
+#         Original word = first_letter + rest_of_word
+#         Append original word to english_list
+
+#     Join english_list with spaces  
+#     Return english_sentence
+# End Function
