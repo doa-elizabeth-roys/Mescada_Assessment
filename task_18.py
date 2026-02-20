8d0cbedd562c8cb31332bc4b77d65bfbc622f42a
Doa Roys <doaelizabethroys@gmail.com>
2026-02-20 15:19:10 +1000

    function that takes a number and returns a list of its digits

---
+++ task_18.py
@@ -0,0 +1,15 @@
+def digit_to_list(digit):
+    digit_list = []
+    digit_len = len(str(digit))
+    
+    while digit > 0:
+        num = digit % 10
+        digit_list.append(num)
+        digit = digit // 10
+    digit_list.reverse()
+
+    return digit_list
+
+
+print(digit_to_list(2342))
+print(digit_to_list(345678))
