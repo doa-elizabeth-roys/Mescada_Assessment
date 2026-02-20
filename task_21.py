6768b74ce32a29b8e3a5f9f8a38a417e4196789d
Doa Roys <doaelizabethroys@gmail.com>
2026-02-20 16:09:56 +1000

    Function to break and print a sentence into multiple lines

---
+++ task_21.py
@@ -0,0 +1,14 @@
+#PSUEDOCODE
+
+# function split_sentence(sentence, max_length):
+#     Split sentence into words - word_list
+#     Create empty string - current_line
+
+#     For each word in word_list:
+#         If adding the word to current_line keeps length ≤ max_length:
+#             - Add word to current_line
+#         Else:
+#             - Print current_line
+#             - Start new current_line with the word
+
+#     After all words, print current_line
